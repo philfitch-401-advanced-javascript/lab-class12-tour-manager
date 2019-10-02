0a3c6f37017971bc442d3189e45f060846f38134
Phil Fitch <mostreluctantly@gmail.com>
2019-10-01 18:48:40 -0700

    tours .post is passing

--- e2e/__tests__/tours.test.js
+++ e2e/__tests__/tours.test.js
@@ -1,5 +1,51 @@
-
+const request = require('../request');
+const db = require('../db');
+const { matchMongoId } = require('../match-helpers');
 
 //POST /api/tours
+describe('tours api', () => {
+  beforeEach(() => {
+    return Promise.all([
+      db.dropCollection('tours'),
+      db.dropCollection('stops')
+    ]);
+  });
+
+  const tour = {
+    title: 'De-Tour',
+    activities: [],
+    launchDate: new Date(),
+    stops: []
+  };
+
+  function postTour(tour) {
+    return request
+      .post('/api/tours')
+      .send(tour)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  it('posts a tour', () => {
+    return postTour(tour).then(show => {
+      expect(show).toMatchInlineSnapshot(
+        {
+          launchDate: expect.any(String),
+          ...matchMongoId
+        },
+        `
+        Object {
+          "__v": 0,
+          "_id": StringMatching /\\^\\[a-f\\\\d\\]\\{24\\}\\$/i,
+          "activities": Array [],
+          "launchDate": Any<String>,
+          "stops": Array [],
+          "title": "De-Tour",
+        }
+      `
+      );
+    });
+  });
+});
 
 //GET /api/tours
